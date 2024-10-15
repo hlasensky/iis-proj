b46c78007feabd904e943287c65d74609f479c11
Jlindovsky <j.lindovsky@icloud.com>
2024-10-15 13:48:38 +0200

    pages hierarchy, miniNav

--- components/MiniNav.tsx
+++ components/MiniNav.tsx
@@ -1,20 +1,25 @@
 "use client";
 
 import {
-	AlignEndVerticalIcon,
-	Calendar,
-	Fingerprint,
-	HomeIcon,
-	LayoutDashboard,
-	Settings,
-	User,
+  AlignEndVerticalIcon,
+  Calendar,
+  Fingerprint,
+  HomeIcon,
+  LayoutDashboard,
+  Settings,
+  User,
 } from "lucide-react";
 
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import SignInButton from "./SignInButton";
 import SignOutButton from "./SignOutButton";
 import { Session } from "next-auth";
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./ui/tooltip";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./ui/tooltip";
 import { cn } from "@/lib/utils";
 import { activeNavAtom } from "@/app/userAtom";
 import { useAtom } from "jotai";
@@ -22,83 +27,85 @@ import { useEffect } from "react";
 import { usePathname } from "next/navigation";
 
 interface NavbarLinkProps {
-	icon: typeof HomeIcon;
-	label: string;
-	active?: boolean;
-	onClick?(): void;
+  icon: typeof HomeIcon;
+  label: string;
+  active?: boolean;
+  onClick?(): void;
 }
 
 function NavbarLink({ icon: Icon, label, active, onClick }: NavbarLinkProps) {
-	return (
-		<TooltipProvider delayDuration={100}>
-			<Tooltip>
-				<TooltipTrigger
-					onClick={onClick}
-					className={cn(
-						active ? "border-2 border-blue-400 text-blue-400" : "",
-						"rounded-full grid place-content-center w-12 h-12 cursor-pointer m-auto"
-					)}
-				>
-					<Icon />
-				</TooltipTrigger>
-				<TooltipContent side={"right"}>{label}</TooltipContent>
-			</Tooltip>
-		</TooltipProvider>
-	);
+  return (
+    <TooltipProvider delayDuration={100}>
+      <Tooltip>
+        <TooltipTrigger
+          onClick={onClick}
+          className={cn(
+            active ? "border-2 border-blue-400 text-blue-400" : "",
+            "rounded-full grid place-content-center w-12 h-12 cursor-pointer m-auto"
+          )}
+        >
+          <Icon />
+        </TooltipTrigger>
+        <TooltipContent side={"right"}>{label}</TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
 }
 
 const mockdata = [
-	{ icon: HomeIcon, label: "Home" },
-	{ icon: LayoutDashboard, label: "Dashboard" },
-	{ icon: AlignEndVerticalIcon, label: "Analytics" },
-	{ icon: Calendar, label: "Releases" },
-	{ icon: User, label: "Account" },
-	{ icon: Fingerprint, label: "Security" },
-	{ icon: Settings, label: "Settings" },
+  { icon: HomeIcon, label: "Home" },
+  { icon: LayoutDashboard, label: "Konference" },
+  { icon: AlignEndVerticalIcon, label: "Prezentace" },
+  { icon: User, label: "Účet" },
+  { icon: Settings, label: "Admin" },
 ];
 
 export function NavbarMinimal({ session }: { session: Session | null }) {
-	const [active, setActive] = useAtom(activeNavAtom);
-	const path = usePathname();
+  const [active, setActive] = useAtom(activeNavAtom);
+  const path = usePathname();
 
-	useEffect(() => {
-		const index = mockdata.findIndex((link) => link.label.toLowerCase() === path.slice(1));
-		if (index !== -1) setActive(index);
-		else setActive(0);
-	}, [path, setActive]);
+  useEffect(() => {
+    const index = mockdata.findIndex(
+      (link) => link.label.toLowerCase() === path.slice(1)
+    );
+    if (index !== -1) setActive(index);
+    else setActive(0);
+  }, [path, setActive]);
 
-	const links = mockdata.map((link, index) => (
-		<NavbarLink
-			{...link}
-			key={link.label}
-			active={index === active}
-			onClick={() => setActive(index)}
-		/>
-	));
+  const links = mockdata.map((link, index) => (
+    <NavbarLink
+      {...link}
+      key={link.label}
+      active={index === active}
+      onClick={() => setActive(index)}
+    />
+  ));
 
-	return (
-		<nav className=" h-screen w-fit bg-slate-100">
-			<div className="grid gap-3">
-				{links}
-				{session ? (
-					<>
-						<Avatar className="m-auto">
-							<AvatarImage
-								src={session?.user?.image || ""}
-								alt={session?.user?.name || "avatar"}
-							/>
-							<AvatarFallback>{session?.user?.name?.charAt(0) || "A"}</AvatarFallback>
-						</Avatar>
+  return (
+    <nav className=" h-screen w-fit bg-slate-100">
+      <div className="flex flex-col">
+        {links}
+        {session ? (
+          <div className="">
+            <Avatar className="m-auto">
+              <AvatarImage
+                src={session?.user?.image || ""}
+                alt={session?.user?.name || "avatar"}
+              />
+              <AvatarFallback>
+                {session?.user?.name?.charAt(0) || "A"}
+              </AvatarFallback>
+            </Avatar>
 
-						<SignOutButton />
-					</>
-				) : (
-					<>
-						<span></span>
-						<SignInButton />
-					</>
-				)}
-			</div>
-		</nav>
-	);
+            <SignOutButton />
+          </div>
+        ) : (
+          <div className="">
+            <span></span>
+            <SignInButton />
+          </div>
+        )}
+      </div>
+    </nav>
+  );
 }
