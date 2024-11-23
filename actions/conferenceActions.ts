af38f0c6213bddc7ea41ecd0b12e1413569e9e99
hlasensky <tomas.hlasensky@seznam.cz>
2024-11-23 09:19:18 +0100

    fix: :bug: fixing non allowed orders

--- actions/conferenceActions.ts
+++ actions/conferenceActions.ts
@@ -92,6 +92,7 @@ export async function getUserConferences() {
                     id: user?.id,
                 },
             },
+            paymentStatus: true,
         },
         include: {
             conference: true,
